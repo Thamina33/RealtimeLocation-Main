1a7211e3c697982108b86c8ab39acb666cd48ffa
Thamina Omar toma <37588746+Thamina33@users.noreply.github.com>
2019-04-16 13:41:35 +0600

    V1.0.9

--- app/src/main/java/com/example/realtime_location/HomeActivity.java
+++ app/src/main/java/com/example/realtime_location/HomeActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,9 +18,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.realtime_location.Interface.IFirebaseLoadDone;
+import com.example.realtime_location.Model.User;
+import com.example.realtime_location.Utils.Common;
+import com.example.realtime_location.ViewHolder.UserViewHOlder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    FirebaseRecyclerAdapter<User, UserViewHOlder> adapter,searchAdapter;
+    RecyclerView recycle_Friend_List;
+    IFirebaseLoadDone firebaseLoadDone;
+
+    MaterialSearchBar searchBar;
+    List<String>suggestList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +64,74 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        View haderView = navigationView.getHeaderView(0);
+        TextView txt_user_logged = (TextView)haderView.findViewById(R.id.txt_logged_email);
+        txt_user_logged.setText(Common.loggedUser.getEmail());
+
+        //view
+
+        //init view
+        searchBar =(MaterialSearchBar)findViewById(R.id.material_search_bar);
+        searchBar.setCardViewElevation(10);
+        searchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                List<String> suggest = new ArrayList<>();
+                for (String search:suggestList){
+                    if(search.toLowerCase().contains(searchBar.getText().toLowerCase()))
+                        suggest.add(search);
+                }
+                searchBar.setLastSuggestions(suggest);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        searchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+                if(!enabled){
+                    if(adapter!=null){
+                        //if close search , restore default
+                        recycle_Friend_List.setAdapter(adapter);
+                    }
+                }
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+
+                startSearch(text.toString());
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+
+            }
+        });
+
+        recycle_Friend_List = (RecyclerView)findViewById(R.id.recycle_Friend_List);
+        recycle_Friend_List.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recycle_Friend_List.setLayoutManager(layoutManager);
+        recycle_Friend_List.addItemDecoration(new DividerItemDecoration(this,((LinearLayoutManager) layoutManager).getOrientation()));
+
     }
+
+    private void startSearch(String toString) {
+
+
+    }
+
 
     @Override
     public void onBackPressed() {
